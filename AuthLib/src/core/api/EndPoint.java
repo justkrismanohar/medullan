a37e5c1cb919423f7345fb2721324831aab42097
Kris Manohar <justkrismanohar@gmail.com>
2019-04-27 23:22:09 -0400

    Added template for EndPoint

--- AuthLib/src/core/api/EndPoint.java
+++ AuthLib/src/core/api/EndPoint.java
@@ -1,17 +1,44 @@
 package core.api;
 
+import java.util.List;
+
 import core.models.LoginRequest;
-
+import core.policy.security.ANDSecurityPolicy;
+import core.policy.security.BasicBruteForce;
+import core.policy.security.LockoutPolicy;
+import core.policy.security.NConsecutiveFailedLogins;
+import core.policy.security.SecurityPolicy;
+import core.policy.security.UserAccountLockedPolicy;;
 public class EndPoint {
-
+	
+	
+		ANDSecurityPolicy blockPolicies;
+		ANDSecurityPolicy postLoginPolicies;
+		
+		public EndPoint() {
+			//should really load execute the following based on .xml config
+			blockPolicies.add(new LockoutPolicy(20));
+			blockPolicies.add(new UserAccountLockedPolicy());
+			
+			postLoginPolicies.add(new NConsecutiveFailedLogins(3));
+			postLoginPolicies.add(new BasicBruteForce(10, 13));
+		}
+		
 		/**
 		 * External APIs will use an EndPoint to access the library
 		 */
 		public boolean login(LoginRequest req) {
 			//Check if the request is blocked
-			//Determine if the UN + PWD pair match
-			//Apply security policies
-			//If pass checks return true
-			return true;
+			if(blockPolicies.handleRequest(req)) {
+				//Determine if the UN + PWD pair match
+				
+				//Apply security post security policies
+				//If pass checks return true	
+				return postLoginPolicies.handleRequest(req);
+			}
+			
+			return false;
 		}
+		
+		
 }
